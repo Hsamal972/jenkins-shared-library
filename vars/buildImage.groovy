e43345f6778a167b2fb80278cede96fa87dce888
Himanshu Samal <39038939+Hsamal972@users.noreply.github.com>
2022-05-06 13:14:18 +0530

    added imageName parameter

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(string imageName) {
     echo "Building the application"
     withCredentials([usernamePassword(credentialsId:'hub-docker-repo',usernameVariable:'USER',passwordVariable:'PASS')]) {
-        sh 'docker build -t doomedmonk13/test1:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push doomedmonk13/test1:jma-2.0'
+        sh "docker push $imageName"
     }
 }
