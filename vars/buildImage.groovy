dac71ef6e222079c9f4bfb06d562557df9531581
Himanshu Samal <39038939+Hsamal972@users.noreply.github.com>
2022-05-06 12:05:28 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "Building the application"
     withCredentials([usernamePassword(credentialsId:'hub-docker-repo',usernameVariable:'USER',passwordVariable:'PASS')]) {
-        sh 'docker build doomedmonk13/test1:jma-2.0 .'
+        sh 'docker build -t doomedmonk13/test1:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push doomedmonk13/test1:jma-2.0'
     }
