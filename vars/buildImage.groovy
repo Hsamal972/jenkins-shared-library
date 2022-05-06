fb831751c37b0ea0b10d4e39bf3fa08594600e5e
Himanshu <doomedmonk13@gmail.com>
2022-05-06 09:49:30 +0530

    first commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "Building the application"
+    withCredentials([usernamePassword(credentialsId:'hub-docker-repo',usernameVariable:'USER',passwordVariable:'PASS')]) {
+        sh 'docker build doomedmonk13/test1:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push doomedmonk13/test1:jma-2.0'
+    }
+}
